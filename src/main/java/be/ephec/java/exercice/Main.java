6f359f02536279397c8cffb71204be185fc13b57
Radoaune <psr12216@students.ephec.be>
2023-01-24 21:02:04 +0100

    Exercice1.1

--- src/main/java/be/ephec/java/exercice/Main.java
+++ src/main/java/be/ephec/java/exercice/Main.java
@@ -1,7 +1,25 @@
 package be.ephec.java.exercice;
 
+import be.ephec.java.exercice.e01.Person;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Person p1=new Person();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("First Name ? : ");
+        p1.firstName = sc.next();
+
+        System.out.println("last Name ? : ");
+        p1.lastName = sc.next();
+
+
+        System.out.println("Age ? : ");
+        p1.age = sc.nextInt();
+
+        p1.display();
     }
 }
